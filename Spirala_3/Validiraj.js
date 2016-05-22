b88ce8cd0ed690a32e53073319900e048ed2b4fb
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2016-05-22 20:18:12 +0200

    Spirala 3

--- Spirala_3/Validiraj.js
+++
@@ -1,4 +0,0 @@
-var imepr=document.getElementById("imeprezime").Value;
-var em=document.getElementById("email").Value;
-var w=document.getElementById("web").Value;
-var kom=document.getElementById("komentar").Value;
